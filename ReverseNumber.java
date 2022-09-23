1c20f74a355b52dd33ab2293c691713881fc7b2d
anjali1742 <113169048+anjali1742@users.noreply.github.com>
2022-09-23 12:52:29 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,16 @@
+//wrp to demo Reverse a Number
+//Anjali Ajith CG Batch 2576
+public class ReverseNumber   
+{  
+public static void main(String[] args)   
+{  
+int number = 9835, reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
