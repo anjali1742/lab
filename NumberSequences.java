208cecc11ae67cfc391fd1041ee9042588fb41d5
anjali1742 <113169048+anjali1742@users.noreply.github.com>
2022-09-23 12:53:56 +0530

    Add files via upload

---
+++ NumberSequences.java
@@ -0,0 +1,37 @@
+//wrp to demo number sequences
+//Anjali Ajith CG BATCH 2576
+
+class NumberSequences
+
+{
+
+public static void main(String[]args)
+
+{
+
+int a = 1;
+
+int b = 4;
+
+int c = 7;
+
+int n = 10;
+
+for(int i = 1; i <= n; i++)
+
+{
+
+int d = a + b + c;
+
+System.out.println(a + " " + b + " " + c + " " + d );
+
+System.out.println(" ");
+
+a = d;
+
+b = b + 1;
+ }
+ 
+ }
+ 
+ }
