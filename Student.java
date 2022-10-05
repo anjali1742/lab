7a5d69e45df1056139b4cf1a990e80d9c7303da2
anjali1742 <113169048+anjali1742@users.noreply.github.com>
2022-10-05 12:08:42 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,23 +1,73 @@
-//Anjali Ajith CG java batch 2576 student details using scanner class
 import java.util.Scanner;
-class Student
+
+public class Student
 {
-	public static void main(String[] args)
-	{
-		String name;
-		int id;
-		float attendennce;
-		Scanner sc = new Scanner(System.in);//System.in is a standard input stream
-		System.out.println("enter the student name:");
-		name = sc.next();
-		System.out.println("enter the student id:");
-		id = sc.nextInt();
-				System.out.println("enter the student attendennce:");
-				attendennce = sc.nextFloat();
-				System.out.println("the name of the student:"+name);
-				System.out.println("the id of the student id:"+id);
-	System.out.println("the attendennce of the student:"+attendennce);
-	}
+    private String name;
+    private int id;
+    private int m1;
+    private int m2;
+    private int m3;
+    private int maximum;
+    private double average;
+
+    public Student(String n, int a, int s1, 
+    int s2, int s3) {
+        name = n;
+        id = a;
+        m1 = s1;
+        m2 = s2;
+        m3 = s3;
+    }
+
+    public Student() {
+        name = "";
+        id = 0;
+        m1 = 0;
+        m2 = 0;
+        m3 = 0;
+        maximum = 0;
+        average = 0;
+    }
+
+    public void accept() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        name = in.nextLine();
+        System.out.print("Enter id: ");
+        id = in.nextInt();
+        System.out.print("Enter Subject 1 Marks: ");
+        m1 = in.nextInt();
+        System.out.print("Enter Subject 2 Marks: ");
+        m2 = in.nextInt();
+        System.out.print("Enter Subject 3 Marks: ");
+        m3 = in.nextInt();
+    }
+
+    public void compute() {
+        if (m1 > m2 && m1 > m3)
+            maximum = m1;
+        else if (m2 > m1 && m2 > m3)
+            maximum = m2;
+        else
+            maximum = m3;
+
+        average = (m1 + m2 + m3) / 3.0;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Id: " + id);
+        System.out.println("Subject 1 Marks: " + m1);
+        System.out.println("Subject 2 Marks: " + m2);
+        System.out.println("Subject 3 Marks: " + m3);
+        System.out.println("Maximum Marks: " + maximum);
+        System.out.println("Average Marks: " + average);
+    }
+
+    public static void main(String args[]) {
+        Student obj = new Student();
+        obj.accept();
+        obj.compute();
+        obj.display();
+    }
 }
-		
-		
