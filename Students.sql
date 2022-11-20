220e190dac643064b1068e80c6b974d00622e3c0
anjali1742 <113169048+anjali1742@users.noreply.github.com>
2022-11-20 14:27:21 +0530

    Add files via upload

---
+++ Students.sql
@@ -0,0 +1,24 @@
+1.prepare students table with sid,sname, marks,course in my sql
+
+
+
+sql query
+
+
+use students
+
+create table student(sid int,sname varchar(30),marks int,course varchar(50))
+
+
+2.insert,update,delete,select operations on student table
+
+insert into  student values(1,'regi',480,'computer science'),(2,'saki',390,'Biology'),(5,'sutha',500,'business')
+
+select * from student
+
+update student set sid=3 where sid=5
+
+delete from student where sid=3
+
+
+
